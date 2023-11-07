62a6d886a18104101e51850da70f9cadfa06b074
Wesley Santos <wesleysantos32892653@outlook.com>
2023-11-07 02:21:29 -0300

    Criando página de Edição

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,8 @@ import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search'
 import Post from './pages/Post/Post'
+import EditPost from './pages/EditPost/EditPost'
+
 
 //CSS
 import './App.css'
@@ -66,6 +68,10 @@ function App() {
           <Route path = "/login" element = {!user ? <Login/> : <Navigate to = "/"/>} />
           <Route path = "/register" element = {!user ? <Register/> : <Navigate to = "/"/>} />
 
+          <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
           <Route path = "/posts/create" element = {user ? <CreatePost/> : <Navigate to = "/login"/>} />
           <Route path = "/dashboard" element = {user ? <Dashboard/> : <Navigate to = "/login"/>} />
 
