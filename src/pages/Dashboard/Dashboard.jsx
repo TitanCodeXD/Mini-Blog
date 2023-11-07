82711ac3503200bdc2673b2f06804ac14296f73d
Wesley Santos <wesleysantos32892653@outlook.com>
2023-11-07 00:47:58 -0300

    Estruturando Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,37 @@
-//React imports
-import React from 'react'
+//React Imports
+import { Link } from "react-router-dom";
+
+//Hooks
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 
 //CSS
 import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
+  const {user} = useAuthValue();
+  const uid = user.id;
+
+
+  //Posts do usuário
+  const posts = []
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie seus posts</p>
+        {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+           <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>) : (
+        <div>
+          s
+        </div>)}
     </div>
   )
 }
